3ce00078b21a6b71395f34f5a10ac95745bdcdd5
Connor <ccarmic2@uccs.edu>
2024-04-07 17:18:20 -0600

    added models, templates, and admin config

--- schedule_app/admin.py
+++ schedule_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CalenderDates, SupplyTask
 
 # Register your models here.
+admin.site.register(CalenderDates)
+admin.site.register(SupplyTask)
--- schedule_app/models.py
+++ schedule_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.urls import reverse
+from datetime import datetime
 
 # Create your models here.
+
+
+class CalenderDates(models.Model):
+    DEFAULT_TIME = "10:00"
+
+    booking_start = models.DateField()
+    booking_end = models.DateField()
+    checkout_time = models.TimeField(DEFAULT_TIME)
+    notes = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return str(self.booking_end)
+
+    def get_absolute_url(self):
+        return reverse("date_detail", args=[str(self.id)])
+
+
+class SupplyTask(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return str(self.name)
+
+    def get_absolute_url(self):
+        return reverse("supply_detail", args=[str(self.id)])
