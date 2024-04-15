9d73ee625e88c14446ade6521ed48c331ffe6633
Connor <ccarmic2@uccs.edu>
2024-04-15 15:40:26 -0600

    view test fix

--- schedule_app/tests/tests_views.py
+++ schedule_app/tests/tests_views.py
@@ -27,15 +27,19 @@ class TestViews(TestCase):
     def test_create_supply_POST(self):
         name = 'second supply'
         desc = 'test desc 2'
+        #get the url to create an object
         create_supply_url = reverse("supply-add")
+        #send a test client response to create a new object
         response = self.client.post(create_supply_url, {
             'name': f'{name}',
             'description': f'{desc}'
         })
-    
+        #get the newly created object (id = 2 for this new object, will differ for you)
         new_supply = SupplyTask.objects.get(id=2)
 
+        #test response code for client redirect
         self.assertEquals(response.status_code, 302)
+        #test object creatation and if it matches the values entered
         self.assertEquals(new_supply.name, name)
         self.assertEquals(new_supply.description, desc)
 
